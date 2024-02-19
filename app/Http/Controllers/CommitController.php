7f1fbce8579bcf2c12cf600c182ad0481bdf8340
Luis-F-Oliveira <luis.f.oliveira505141@outlook.com>
2024-02-19 16:58:30 -0400

    database

---
+++ app/Http/Controllers/CommitController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Commit;
+use Illuminate\Http\Request;
+
+class CommitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Commit $commit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Commit $commit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Commit $commit)
+    {
+        //
+    }
+}
