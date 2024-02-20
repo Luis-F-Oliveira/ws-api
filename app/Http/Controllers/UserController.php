16454e618095a3830c99f6603d7637ad4cba9b38
Luis-F-Oliveira <luis.f.oliveira505141@outlook.com>
2024-02-20 14:36:48 -0400

    UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,27 +2,76 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index()
     {
-        //
+        try {
+            return User::all();
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ]);
+        }
     }
 
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        try {
+            return User::findOrFail($id);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ]);
+        }
     }
 
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        try {
+            $user = User::find($id);
+
+            if ($user) {
+                $user->name = $request->input('name');
+                $user->email = $request->input('email');
+                $user->password = Hash::make($request->input('password'));
+
+                $user->save();
+
+                return response()->json([
+                    'message' => 'User Updated'
+                ], 200);
+            }
+
+            return response()->json([
+                'message' => 'User Not Found'
+            ], 401);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ]);
+        }
     }
 
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        try {
+            $user = User::findOrFail($id);
+
+            $user->delete();
+
+            return response()->json([
+                'message' => 'User Deleted'
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ]);
+        }
     }
 }
